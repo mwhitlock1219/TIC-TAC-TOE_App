0f59c88534b2bb1caf4630246143147e9a269946
Melina Alejos <malejos1219@gmail.com>
2020-04-27 13:44:25 -0400

    Updates

---
+++ precache-manifest.f47137193425fe04041fcf33429231d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09258953f55adfea672a3580f1c2f2d6",
+    "url": "/TIC-TAC-TOE_App/index.html"
+  },
+  {
+    "revision": "96c570fafab98509d072",
+    "url": "/TIC-TAC-TOE_App/static/css/main.00cd9408.chunk.css"
+  },
+  {
+    "revision": "decb4e1e1cd60822aef5",
+    "url": "/TIC-TAC-TOE_App/static/js/2.239b4334.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TIC-TAC-TOE_App/static/js/2.239b4334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96c570fafab98509d072",
+    "url": "/TIC-TAC-TOE_App/static/js/main.8d7cdd78.chunk.js"
+  },
+  {
+    "revision": "d27e6e283cc6c2c0b2db",
+    "url": "/TIC-TAC-TOE_App/static/js/runtime-main.11c95672.js"
+  }
+]);
